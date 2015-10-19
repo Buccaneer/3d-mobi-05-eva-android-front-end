7d05cab119d99c379a08223a21fa27f5eb47a9da
Jan Van Braeckel <jan.vanbraeckel.x7253@student.hogent.be>
2015-10-19 22:42:39 +0200

    WORK IN PROGRESS Added a lot of layout elements to the main menu, images used and styles are purely representational and will be edited of course

--- EVA21DayChallenge/app/src/main/java/evavzw/be/eva21daychallenge/activity/MainMenu.java
+++ EVA21DayChallenge/app/src/main/java/evavzw/be/eva21daychallenge/activity/MainMenu.java
@@ -2,23 +2,41 @@ package evavzw.be.eva21daychallenge.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import evavzw.be.eva21daychallenge.R;
 import evavzw.be.eva21daychallenge.activity.base.RESTfulActivity;
 import evavzw.be.eva21daychallenge.security.UserManager;
 
 public class MainMenu extends RESTfulActivity {
-    private UserManager userManager;
+    /*@Bind(R.id.btnChallenges)
+    Button btnChallenges;
+    @Bind(R.id.btnEvaSite)
+    Button btnEvaSite;
+    @Bind(R.id.btnRecipes)
+    Button btnRecipes;
+    @Bind(R.id.btnPH1)
+    Button btnPH1;
+    @Bind(R.id.btnPH2)
+    Button btnPH2;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.setContentResId(R.layout.activity_main_menu);
         super.onCreate(savedInstanceState);
 
-        userManager = UserManager.getInstance(getApplicationContext());
         ButterKnife.bind(this);
+/*
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+  */
 
     }
 
@@ -27,27 +45,5 @@ public class MainMenu extends RESTfulActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_with_actions, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if(id == R.id.logOut){
-            userManager.invalidateToken();
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            finish();
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- EVA21DayChallenge/app/src/main/java/evavzw/be/eva21daychallenge/activity/base/RESTfulActivity.java
+++ EVA21DayChallenge/app/src/main/java/evavzw/be/eva21daychallenge/activity/base/RESTfulActivity.java
@@ -2,6 +2,7 @@ package evavzw.be.eva21daychallenge.activity.base;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import evavzw.be.eva21daychallenge.R;
+import evavzw.be.eva21daychallenge.activity.Login;
+import evavzw.be.eva21daychallenge.security.UserManager;
 
 /**
  * Created by Jan on 17/10/2015.
@@ -24,12 +27,14 @@ public abstract class RESTfulActivity extends AppCompatActivity {
 
     private int mContentResId;
     private MenuItem menuItem;
+    private UserManager userManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(mContentResId);
-	}
+        userManager = UserManager.getInstance(getApplicationContext());
+    }
 
     protected void setContentResId(int id) {
         mContentResId = id;
@@ -40,6 +45,28 @@ public abstract class RESTfulActivity extends AppCompatActivity {
         menuItem = menu.findItem(R.id.menu_progress);
         ProgressBar progressBar = (ProgressBar) MenuItemCompat.getActionView(menuItem);
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id == R.id.logOut){
+            userManager.invalidateToken();
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            finish();
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void toggleProgressBar(boolean toggle){
