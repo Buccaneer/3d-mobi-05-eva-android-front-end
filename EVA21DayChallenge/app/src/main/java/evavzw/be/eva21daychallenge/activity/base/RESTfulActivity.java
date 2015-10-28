190f85b67c18f20770d19cccbd6f1625c8d77c1e
Jan Van Braeckel <jan.vanbraeckel.x7253@student.hogent.be>
2015-10-28 23:36:36 +0100

    Accidently deleted too much in the merge conflict. This was such a minor change that I didn't bother making a branch.

--- EVA21DayChallenge/app/src/main/java/evavzw/be/eva21daychallenge/activity/base/RESTfulActivity.java
+++ EVA21DayChallenge/app/src/main/java/evavzw/be/eva21daychallenge/activity/base/RESTfulActivity.java
@@ -16,8 +16,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -32,8 +34,8 @@ import evavzw.be.eva21daychallenge.security.UserManager;
 public abstract class RESTfulActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private int mContentResId;
-    private MenuItem menuItem;
     private UserManager userManager;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +56,26 @@ public abstract class RESTfulActivity extends AppCompatActivity implements Navig
             drawer.setDrawerListener(toggle);
             toggle.syncState();
         }
+
+        progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+        progressBar.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 34));
+        progressBar.setProgress(65);
+        final FrameLayout decorView = (FrameLayout) getWindow().getDecorView();
+        decorView.addView(progressBar);
+
+        ViewTreeObserver observer = progressBar.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                View contentView = decorView.findViewById(android.R.id.content);
+                progressBar.setY(contentView.getY() +55);
+
+                ViewTreeObserver observer = progressBar.getViewTreeObserver();
+                observer.removeGlobalOnLayoutListener(this);
+            }
+        });
+        progressBar.setIndeterminate(true);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 
     protected void setContentResId(int id) {
@@ -100,14 +122,7 @@ public abstract class RESTfulActivity extends AppCompatActivity implements Navig
         }
     }
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menuItem = menu.findItem(R.id.menu_progress);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
     public void toggleProgressBar(boolean toggle) {
-        if (menuItem != null)
-            menuItem.setVisible(toggle);
+        progressBar.setVisibility(toggle ? View.VISIBLE : View.INVISIBLE);
     }
 }
